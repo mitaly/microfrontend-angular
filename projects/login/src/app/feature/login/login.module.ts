b7f4199f955b59e4d6bc7c3a6246f55836d1b3f2
sshubhm <senmitaly13@gmail.com>
2023-12-19 17:37:58 +0530

    create a login angular app inside an angular app

---
+++ projects/login/src/app/feature/login/login.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+import { LoginComponent } from './login.component';
+
+
+const routes: Route[] = [
+  {
+    path: "",
+    component: LoginComponent
+  }
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class LoginModule { }
